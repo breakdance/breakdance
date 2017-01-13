841e81aab34f53bb9eac4eb54c1429d4a8316f7d
jonschlinkert <jon.schlinkert@sellside.com>
2017-01-13 06:35:17 -0500

    adds gulp for coverage/tests

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var eslint = require('gulp-eslint');
+var unused = require('gulp-unused');
+
+gulp.task('coverage', function() {
+  return gulp.src(['index.js', 'lib/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['coverage'], function() {
+  return gulp.src('test/*.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports());
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['*.js', 'lib/**/*.js', 'test/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('unused', function() {
+  var keys = Object.keys(require('./lib/utils.js'));
+  keys = keys.concat(Object.keys(require('./lib/helpers.js')));
+  return gulp.src(['index.js', 'lib/**/*.js'])
+    .pipe(unused({keys: keys}));
+});
+
+gulp.task('default', ['test', 'lint']);
